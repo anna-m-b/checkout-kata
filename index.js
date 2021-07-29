22a4ee954fdd2163da568edf991ee4868f56b1a3
Anna Balquin <anna.balquin@gmail.com>
2021-07-29 16:24:35 +0100

    expected functionality catered for

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // I want to scan a product to know its price //
 // I want to scan many products to know their total price //
-// I want discounts to be applied if there are multibuy offers,
+// I want discounts to be applied if there are multibuyQuantity offers,
 // when the total is finally calculated.
 // https://freesoft.dev/program/113094459
 
@@ -14,46 +14,96 @@
 // D 15
 
 // The pricing structure should be separate from the checkout (and passed in)
-
-let scannedItems = {};
-
-let pricingRules = [
+// const unitPricingChart = {
+//   //   kiwi: 50,
+//   banana: 32,
+//   apple: 48,
+// };
+//const calculateTotal = () => {
+//   return scannedItems.reduce((acc, cur) => {
+//     return (acc += cur);
+//   });
+// };
+const pricingRules = [
   {
     item: "kiwi",
-    quantity: 3,
-    discount: -20,
+    price: 50,
+    multibuyQuantity: 3,
+    discount: 20,
+  },
+  {
+    item: "banana",
+    price: 32,
+    multibuyQuantity: 1,
+    discount: 0,
+  },
+  {
+    item: "apple",
+    price: 48,
+    multibuyQuantity: 1,
+    discount: 0,
   },
 ];
 
-const clearBasket = () => {
-  scannedItems = {};
+class checkout {
+  constructor(pricingRules) {
+    this.basket = {}
+    this.pricingRules = pricingRules
+    this.total = 0;
+  }
+  scan(item) {
+    this.basket[item] = ++this.basket[item] || 1;
+  }
+  calculateMultibuyDiscount(item, quantity) {
+    const { multibuyQuantity, discount } = this.findPricingRule(item)
+    let discountToApply = 0;
+    while (quantity >= multibuyQuantity) {
+      discountToApply += discount
+      quantity -= multibuyQuantity
+    }
+    return discountToApply;
+  }
+  calculateTotal() {
+    for (const [item, quantity] of Object.entries(this.basket)) {
+      const discount = this.calculateMultibuyDiscount(item, quantity);
+      this.total += this.findPricingRule(item).price * quantity - discount;
+    }
+    return this.total;
+  }
+ findPricingRule(item) {
+    return this.pricingRules.find((rule) => rule.item == item)
+  }
+  clearBasket () {
+    this.basket = {}
+  }
+}
+
+// const clearBasket = () => {
+//   basket = {};
+// };
+
+// const calculateMultibuyDiscount = (item, quantity) => {
+//   const pricingRule = pricingRules.find((rule) => rule.item == item);
+//   return pricingRule.multibuyQuantity === quantity ? pricingRule.discount : 0;
+// };
+
+// // const scan = (item) => {
+// //   basket[item] = ++basket[item] || 1;
+// //   console.log(basket)
+// // };
+
+// const calculateTotal = () => {
+//   let total = 0;
+//   for (const [item, quantity] of Object.entries(basket)) {
+//     const discount = calculateMultibuyDiscount(item, quantity);
+//     total +=
+//       pricingRules.find((rule) => rule.item == item).price * quantity +
+//       discount;
+//   }
+//   return total;
+// };
+
+module.exports = {
+  checkout,
+  pricingRules
 };
-
-const applyAnyDiscounts = () => {
-  // loop through the scanned items object keys,
-  // if one of the keys is in the pricingRules,
-  // we want to count the number of scans to see if it meets the discount
-};
-
-const unitPricingChart = {
-  kiwi: 50,
-  banana: 32,
-  apple: 48,
-};
-
-const scan = (item) => {
-  let value = unitPricingChart[item];
-
-  scannedItems[item] = ++scannedItems[item] || 1;
-
-  console.log("Scanned items", scannedItems);
-  return value;
-};
-
-const calculateTotal = () => {
-  return scannedItems.reduce((acc, cur) => {
-    return (acc += cur);
-  });
-};
-
-module.exports = { scan, calculateTotal, clearBasket, applyAnyDiscounts };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
-// I want to scan a product to know its price
-// I want to scan many products to know their total price
+// I want to testCheckout.scan a product to know its price
+// I want to testCheckout.scan many products to know their total price
 // I want discounts to be applied if there are multibuy offers, when the total is finally calculated
 
 // SKU Unit Price Special Price
@@ -13,47 +13,55 @@
 // ARRANGE / ACT / ASSERT
 
 const {
-  scan,
-  calculateTotal,
-  clearBasket,
-  applyAnyDiscounts,
+checkout,
+pricingRules
 } = require("./index");
 
-afterEach(() => {
-  clearBasket();
+let testCheckout;
+beforeEach(() => {
+  testCheckout = new checkout(pricingRules)
+})
+
+test("testCheckout.scan should add item and quantity to the testCheckout.scannedItemsQuantity object", () => {
+  // ARRANGE
+  const expected = {banana: 1}
+  // ACT
+  testCheckout.scan("banana");
+  // ASSERT
+  expect(testCheckout.basket).toEqual(expected);
 });
 
-test("I want to scan a product to know its price", () => {
+test("scan can handle multiple items", () => {
   // ARRANGE
-  const expected = 32;
-
+  const expected = {apple: 2, banana: 1}
   // ACT
-  const checkoutScan = scan("banana");
-
+ testCheckout.scan("apple");
+ testCheckout.scan("banana");
+ testCheckout.scan("apple");
   // ASSERT
-  expect(checkoutScan).toBe(expected);
-});
-
-test("I want to scan a different product to know its price", () => {
-  // ARRANGE
-  const expected = 48;
-
-  // ACT
-  const checkoutScan = scan("apple");
-
-  // ASSERT
-  expect(checkoutScan).toBe(expected);
+  expect(testCheckout.basket).toEqual(expected);
 });
 
 test("If I scan 2 items, the checkout tells me the total price", () => {
   //ARRANGE
   const expected = 80;
-  scan("apple");
-  scan("banana");
-
+  testCheckout.scan("apple");
+  testCheckout.scan("banana");
   // ACT
-  const checkoutTotal = calculateTotal();
+  const checkoutTotal = testCheckout.calculateTotal();
+  // ASSERT
+  expect(checkoutTotal).toBe(expected);
+});
 
+test("If I scan multiple items, the checkout tells me the total price", () => {
+  //ARRANGE
+  const expected = 162;
+  testCheckout.scan("apple");
+  testCheckout.scan("banana");
+  testCheckout.scan("banana");
+  testCheckout.scan("kiwi");
+  // ACT
+  const checkoutTotal = testCheckout.calculateTotal();
   // ASSERT
   expect(checkoutTotal).toBe(expected);
 });
@@ -61,13 +69,33 @@ test("If I scan 2 items, the checkout tells me the total price", () => {
 test("If I scan enough of the same items that are on a multi-buy, the discount is applied", () => {
   // ARRANGE
   const expected = 130;
-  scan("kiwi");
-  scan("kiwi");
-  scan("kiwi");
-    
+  testCheckout.scan("kiwi");
+  testCheckout.scan("kiwi");
+  testCheckout.scan("kiwi");
   // ACT
-  const checkoutDiscountedTotal = applyAnyDiscounts();
-
+  const checkoutDiscountedTotal = testCheckout.calculateTotal();
   // ASSERT
   expect(checkoutDiscountedTotal).toBe(expected);
 });
+
+test("Discount is accurately applied to items when more than the multibuy quantity are bought", () => {
+  //ARRANGE
+  const expected = 180;
+  testCheckout.scan("kiwi");
+  testCheckout.scan("kiwi");
+  testCheckout.scan("kiwi");
+  testCheckout.scan("kiwi");
+  // ACT
+  const checkoutDiscountedTotal = testCheckout.calculateTotal();
+  // ASSERT
+  expect(checkoutDiscountedTotal).toBe(expected);
+})
+
+test("checkout handles case of item missing from pricing rules gracefully", () => {
+  //ARRANGE
+  testCheckout.scan("unknownItem");
+  //ACT
+  const checkoutUnknownItem = testCheckout.calculateTotal();
+  //ASSERT
+  console.log(checkoutUnknownItem)
+})
